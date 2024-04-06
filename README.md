b961d674f64dc8a583009eead95edcb123693741
Shailendra Vikas <vikas@trdlnk.com>
2024-04-06 15:37:41 -0500

    some misc update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # rbi_state_finance
+1) Download raw files from this page
+https://rbi.org.in/Scripts/AnnualPublications.aspx?head=Handbook%20of%20Statistics%20on%20Indian%20Economy
+2) Arrange in database or database like structure
+3) Make some library to do data analysis
+4) Make some plots
--- source_code/download_raw_data.py
+++ source_code/download_raw_data.py
@@ -4,15 +4,17 @@ import os
 import json
 from urllib.request import urlopen, urlretrieve
 
+handbook_urls = {
+    'Economy': r"https://rbi.org.in/Scripts/AnnualPublications.aspx?head=Handbook%20of%20Statistics%20on%20Indian%20Economy",
+    'States': r"https://www.rbi.org.in/Scripts/AnnualPublications.aspx?head=Handbook%20of%20Statistics%20on%20Indian%20States"
+}
 
-def extract_handbook_info():
+
+def extract_handbook_info(topic: str):
     """
         Extract info from
-        https://rbi.org.in/Scripts/AnnualPublications.aspx?head=Handbook%20of%20Statistics%20on%20Indian%20Economy
     """
-    handbook_url = \
-        r"https://rbi.org.in/Scripts/AnnualPublications.aspx?head=Handbook%20of%20Statistics%20on%20Indian%20Economy"
-
+    handbook_url = handbook_urls[topic]
     page = urlopen(handbook_url)
     html_str = page.read().decode("utf-8")
     table_title_url = []
@@ -36,41 +38,41 @@ def extract_handbook_info():
     return table_title_url
 
 
-def create_title_file(raw_data_dir: str):
+def create_title_file(raw_data_dir: str, topic: str):
     import glob
     all_xlsx = set([full_path.rpartition('/')[2] for full_path in glob.glob(os.path.join(raw_data_dir, "*.XLSX"))])
     current_title = {}
-    for _title, _url in extract_handbook_info():
+    for _title, _url in extract_handbook_info(topic):
         xlsx_filename = _url.rpartition('/')[2]
         if xlsx_filename in all_xlsx:
             current_title[xlsx_filename] = _title
 
     print(all_xlsx)
     print(f"current_title: {current_title}")
-    title_file_name = os.path.join(raw_data_dir, 'title.json')
+    title_file_name = os.path.join(raw_data_dir, topic, 'title.json')
     with open(title_file_name, "w") as current_file_object:
         current_file_object.write(json.dumps(current_title))
     return current_title
 
 
-def download_all_files(raw_data_dir: str):
+def download_all_files(raw_data_dir: str, topic: str):
     # Read the title.json file
-    title_file_name = os.path.join(raw_data_dir, 'title.json')
+    title_file_name = os.path.join(raw_data_dir, topic,  'title.json')
     try:
         with open(title_file_name) as title_file:
             current_title = json.loads(title_file.read())
     except FileNotFoundError:
-        current_title = create_title_file(raw_data_dir)
+        current_title = create_title_file(raw_data_dir, topic)
 
     # Get info from website and download if does not exit
     source_title = {}
-    for _title, _url in extract_handbook_info():
+    for _title, _url in extract_handbook_info(topic):
         xlsx_filename = _url.rpartition('/')[2]
         source_title[xlsx_filename] = _title
         # Find out which all files are missing and download
         if xlsx_filename not in current_title:
             print(f"Downloading file {xlsx_filename}")
-            urlretrieve(_url, os.path.join(raw_data_dir, xlsx_filename))
+            urlretrieve(_url, os.path.join(raw_data_dir, topic, xlsx_filename))
         current_title[xlsx_filename] = _title
         print("Writing ")
 
@@ -79,20 +81,22 @@ def download_all_files(raw_data_dir: str):
 
 
 def test():
+    topic = 'States'
     raw_data_dir = os.path.join('/home/vikas/personal_repository/rbi_state_finance/raw_data/')
-    download_all_files(raw_data_dir)
+    download_all_files(raw_data_dir, topic)
 
 
 def print_file():
-    for index, (_title, _url) in enumerate(extract_handbook_info()):
+    topic = 'States'
+    for index, (_title, _url) in enumerate(extract_handbook_info(topic)):
         print(f"Row {index} \'{_title}\')")
         print(f"    {_url}")
 
 
 if __name__ == "__main__":
     #print_file()
-    #test()
-    create_title_file('/home/vikas/personal_repository/rbi_state_finance/raw_data/')
+    test()
+    #create_title_file('/home/vikas/personal_repository/rbi_state_finance/raw_data/')
 
 
 
